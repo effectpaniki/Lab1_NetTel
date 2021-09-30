3984555d4f0a2b7813c66ea113b0bff042e35dd6
effectpaniki <drakarys44@mail.ru>
2021-09-30 15:50:18 +0700

    Update apps

--- First_APP/First_APP/First_APP.cpp
+++ First_APP/First_APP/First_APP.cpp
@@ -12,43 +12,64 @@ int main() {
 		return 1;
 	}
 
-	int client_socket1 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
 	SOCKADDR_IN sin;
 	sin.sin_family = AF_INET;
-	sin.sin_port = htons(20000);
 
-	inet_pton(AF_INET, "192.168.0.100", &sin.sin_addr.s_addr);
+	inet_pton(AF_INET, "127.0.0.1", &sin.sin_addr.s_addr);
 
-	int connection1 = connect(client_socket1, (LPSOCKADDR)&sin, sizeof(sin));
-	if (connection1 < 0) cout << "Error with connection!" << endl;
-	else cout << "Succesfull connection!" << endl;
-
-	cout << "Client1 -> Client2" << endl;
-	char send_msg[] = "Hello from client1!";
-	send(client_socket1, send_msg, sizeof(send_msg), 0);
-	closesocket(client_socket1);
-
+	int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	sin.sin_port = htons(15000);
-	inet_pton(AF_INET, "192.168.0.100", &sin.sin_addr.s_addr);
 
-	err = bind(server_socket, (LPSOCKADDR)&sin, sizeof(sin));
-	if (err < 0) cout << "Linking error!" << endl;
-	err = listen(server_socket, SOMAXCONN);
+	int connection1, server_socket1, size;
 
 	SOCKADDR_IN from;
 	int fromlen = sizeof(from);
-	int server_socket1 = accept(server_socket, (struct sockaddr*)&from, &fromlen);
+
+	sin.sin_port = htons(15000);
+	err = bind(server_socket, (LPSOCKADDR)&sin, sizeof(sin));
+	if (err < 0) cout << "Linking error!" << endl;
+
+	sin.sin_port = htons(20000);
+	connection1 = connect(client_socket, (LPSOCKADDR)&sin, sizeof(sin));
+	if (connection1 < 0) cout << "Error with connection!" << endl;
+	else cout << "Succesfull connection!" << endl;
+
+	sin.sin_port = htons(15000);
+	err = listen(server_socket, SOMAXCONN);
+	server_socket1 = accept(server_socket, (struct sockaddr*)&from, &fromlen);
 	if (server_socket1 < 0) cout << "Error with accepting!" << endl;
 
-	cout << "Client1 <- Client3" << endl;
-	char* received_msg = new char[255];
-	int size = recv(server_socket1, received_msg, 255, 0);
-	cout << received_msg << " Length of received string = " << size << endl;
+	char x = 'y';
+	int i = 1;
+	char send_msg[] = "Hello from client1!";
 
-	delete[] received_msg;
+	while (x != 'n') {
+		cout << "--------Iteration " << i <<"--------" <<endl;
+		char* received_msg = new char[255];
+		char* buffer = new char[255];
+
+		cout << "Client1 -> Client2" << endl;
+		send(client_socket, send_msg, sizeof(send_msg), 0);
+
+		cout << "Client1 <- Client3" << endl;
+		size = recv(server_socket1, received_msg, 255, 0);
+		cout << received_msg << " Length of received string = " << size << endl;
+		cout << "Client's Port = " << ntohs(from.sin_port) << endl;
+		cout << "Client's IP address = " << inet_ntop(AF_INET, &from.sin_addr, buffer, 255) << endl;
+
+		delete[] received_msg;
+		delete[] buffer;
+
+		i++;
+		cout << "\nDo you want to continue? y/n" << endl;
+		cin >> x;
+		cout << "\n";
+	}
+	cout << "Server has stopped his work!" << endl;
+	send(client_socket, "Server has stopped his work!", 28, 0);
+
 	closesocket(server_socket1);
-
+	closesocket(client_socket);
 	closesocket(server_socket);
+
 }
--- Third_APP/Third_APP/Third_APP.cpp
+++ Third_APP/Third_APP/Third_APP.cpp
@@ -14,45 +14,64 @@ int main() {
 	}
 
 	int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	SOCKADDR_IN sin;
 	sin.sin_family = AF_INET;
-	sin.sin_port = htons(25000);
 
-	inet_pton(AF_INET, "192.168.0.100", &sin.sin_addr.s_addr);
-
-	err = bind(server_socket, (LPSOCKADDR)&sin, sizeof(sin));
-	if (err < 0) cout << "Linking error!" << endl;
-	err = listen(server_socket, SOMAXCONN);
 
 	SOCKADDR_IN from;
 	int fromlen = sizeof(from);
-	int server_socket1 = accept(server_socket, (struct sockaddr*)&from, &fromlen);
+
+	int server_socket1, size, connection1;
+
+	inet_pton(AF_INET, "127.0.0.1", &sin.sin_addr.s_addr);
+
+	sin.sin_port = htons(25000);
+	err = bind(server_socket, (LPSOCKADDR)&sin, sizeof(sin));
+	if (err < 0) cout << "Linking error!" << endl;
+
+	sin.sin_port = htons(25000);
+	err = listen(server_socket, SOMAXCONN);
+	server_socket1 = accept(server_socket, (struct sockaddr*)&from, &fromlen);
 	if (server_socket1 < 0) cout << "Error with accepting!" << endl;
 
-	cout << "Client3 <- Client2" << endl;
-	char* received_msg = new char[255];
-	int size = recv(server_socket1, received_msg, 255, 0);
-	closesocket(server_socket1);
-
-	cout << received_msg << " Length of received string = " << size << endl;
-	cout << "Client's Port = " << ntohs(from.sin_port) << endl;
-	char* buffer = new char[255];
-	cout << "Client's IP address = " << inet_ntop(AF_INET, &from.sin_addr, buffer, 255) << endl;
-	delete[] buffer;
-
-	int client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	sin.sin_port = htons(15000);
-
-	int connection1 = connect(client_socket, (LPSOCKADDR)&sin, sizeof(sin));
+	connection1 = connect(client_socket, (LPSOCKADDR)&sin, sizeof(sin));
 	if (connection1 < 0) cout << "Error with connection!" << endl;
 	else cout << "Succesfull connection!" << endl;
 
-	cout << "Client3 -> Client1" << endl;
-	send(client_socket, received_msg, size, 0);
-	closesocket(client_socket);
+	int i = 1;
+	while (1) {
+		cout << "--------Iteration " << i << "--------" << endl;
+		char* received_msg = new char[255];
+		char* buffer = new char[255];
 
-	delete[] received_msg;
+		cout << "Client3 <- Client2" << endl;
+		size = recv(server_socket1, received_msg, 255, 0);
+
+		if (strcmp(received_msg, "Server has stopped his work!;") == 0) {
+			delete[] received_msg;
+			delete[] buffer;
+			break;
+		}
+
+		cout << received_msg << " Length of received string = " << size << endl;
+		cout << "Client's Port = " << ntohs(from.sin_port) << endl;
+		cout << "Client's IP address = " << inet_ntop(AF_INET, &from.sin_addr, buffer, 255) << endl;
+
+		cout << "Client3 -> Client1" << endl;
+		send(client_socket, received_msg, size, 0);
+
+		delete[] received_msg;
+		delete[] buffer;
+
+		i++;
+		cout << "\n";
+	}
+	cout << "Server has stopped his work!" << endl;
+
+	closesocket(server_socket1);
+	closesocket(client_socket);
 	closesocket(server_socket);
 }
-
